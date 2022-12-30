e4ecf1447bf573506fa1d0432ce12bb9135320d3
Berat Kir <beratkir61@gmail.com>
2022-12-30 18:44:08 +0100

    mintwallets now works correctly, it is still sequentially minting per wallet

--- mintwallets.js
+++ mintwallets.js
@@ -6,16 +6,23 @@ const rawData = fs.readFileSync("keys.json");
 
 const privateKeys = JSON.parse(rawData).keys;
 
+console.log("Private keys length: " + privateKeys.length);
+
 const abi = ["function mint(uint256 quantity) external payable"];
 
 const contractAddress = process.env.CONTRACT_ADDRESS;
 const amountToMint = process.env.AMOUNT_TO_MINT;
 const ethToSent = process.env.PAYABLE;
+const NETWORK = process.env.TESTNET.toLowerCase() === "true" ? "goerli" : "homestead";
 
 const overrides = { value: ethers.utils.parseEther(ethToSent) };
 
 async function main() {
-  const provider = ethers.getDefaultProvider();
+  console.log(`contract address: ${contractAddress} on: ${NETWORK}`);
+  const provider = ethers.getDefaultProvider(NETWORK, {
+    etherscan: process.env.ETHERSCAN_API_KEY,
+    alchemy: process.env.ALCHEMY_API_KEY,
+  });
 
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
@@ -23,11 +30,25 @@ async function main() {
     const signer = new ethers.Wallet(privateKeys[i]).connect(provider);
     const signedContract = contract.connect(signer);
 
+    let tx;
+    const balanceInEth = ethers.utils.formatEther(await signer.getBalance());
+
+    console.log(`Wallet: ${signer.address}, balance: ${balanceInEth}`);
+
     try {
-      const a = await signedContract.mint(amountToMint, overrides);
-      console.log(`${signer.address} minted succesfully!`);
+      tx = await signedContract.mint(amountToMint, overrides);
     } catch (err) {
-      console.log(err);
+      if (err.code === "INSUFFICIENT_FUNDS") {
+        console.log(`   failed to mint, insufficient funds`);
+      }
+
+      if (err.code === "UNPREDICTABLE_GAS_LIMIT") {
+        console.log(`    failed to mint, unpredictable gas limit balance`);
+      }
+    }
+
+    if (tx !== undefined) {
+      console.log(tx);
     }
   }
 }
